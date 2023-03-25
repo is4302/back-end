0f118d84d2c14d10fd086618687ee3dcbba827d2
zt <58112399+zhengtaoJ@users.noreply.github.com>
2023-03-25 21:29:18 +0800

    Revert "Connect with next.js frontend"
    This reverts commit 36782858fe506c95107388a32c080e7fe64a3c10.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -39,8 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     'prescriptions',
     'corsheaders',
-    'rest_framework',
-    'django_nextjs'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,7 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from prescriptions import views
-from prescriptions.views import index
 
 router = routers.DefaultRouter()
 router.register(r'prescriptions', views.PrescriptionView, 'prescription')
@@ -26,7 +25,5 @@ router.register(r'doctors', views.DoctorView, 'doctor')
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/", include(router.urls)),
-    path("", include("django_nextjs.urls")),
-    path("", index, name="index")
+    path("api/", include(router.urls))
 ]
