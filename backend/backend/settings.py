1a86afae5aabc5b0a9ca27f66ae192d584cdcea0
CWei88 <chenwei5lv@gmail.com>
2023-04-07 21:39:23 +0800

    Test

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -141,11 +142,47 @@ CORS_ORIGIN_WHITELIST = [
     'http://localhost:3000'
 ]
 
-REST_FRAMEWORK = {'DEFAULT_PERMISSION_CLASSES':['rest_framework.permissions.AllowAny'],
-                  'DEFAULT_AUTHENTICATION_CLASSES': (
-                        'rest_framework.authentication.TokenAuthentication',
-                        'rest_framework.authentication.BasicAuthentication',
-                )}
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES':['rest_framework.permissions.IsAuthenticated'],
+    'DEFAULT_AUTHENTICATION_CLASSES': (        
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+                  )}
+
+JWT_AUTH = {
+    'JWT_ENCODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_encode_handler',
+
+    'JWT_DECODE_HANDLER':
+    'rest_framework_jwt.utils.jwt_decode_handler',
+
+    'JWT_PAYLOAD_HANDLER':
+    'rest_framework_jwt.utils.jwt_payload_handler',
+
+    'JWT_PAYLOAD_GET_USER_ID_HANDLER':
+    'rest_framework_jwt.utils.jwt_get_user_id_from_payload_handler',
+
+    'JWT_RESPONSE_PAYLOAD_HANDLER':
+    'rest_framework_jwt.utils.jwt_response_payload_handler',
+
+    'JWT_GET_USER_SECRET_KEY': None,
+    'JWT_PUBLIC_KEY': None,
+    'JWT_PRIVATE_KEY': None,
+    'JWT_ALGORITHM': 'HS256',
+    'JWT_VERIFY': True,
+    'JWT_VERIFY_EXPIRATION': True,
+    'JWT_LEEWAY': 0,
+    'JWT_EXPIRATION_DELTA': timedelta(days=3),
+    'JWT_AUDIENCE': None,
+    'JWT_ISSUER': None,
+
+    'JWT_ALLOW_REFRESH': False,
+    'JWT_REFRESH_EXPIRATION_DELTA': timedelta(days=3),
+
+    'JWT_AUTH_HEADER_PREFIX': 'JWT',
+    'JWT_AUTH_COOKIE': None,
+}
 
 AUTH_USER_MODEL = 'prescriptions.User'
 
--- backend/prescriptions/serializers.py
+++ backend/prescriptions/serializers.py
@@ -1,5 +1,44 @@
 from rest_framework import serializers
+from rest_framework_jwt.settings import api_settings
 from .models import Prescription, PatientInformation, DoctorInformation, Appointment, User
+
+jwt_payload_handler = api_settings.JWT_PAYLOAD_HANDLER
+jwt_encode_handler = api_settings.JWT_ENCODE_HANDLER
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email']
+
+class UserSeralizerToken(serializers.ModelSerializer):
+    token = serializers.SerializerMethodField()
+    password = serializers.CharField(write_only=True)
+    
+    class Meta:
+        model = User
+        fields = ('token', 'email', 'password', 'profile')
+
+    def get_token(self, response): ##use to get user
+        payload = jwt_payload_handler(response)
+        token = jwt_encode_handler(payload) 
+        return token
+    
+    def create(self, data):
+        profile = data.pop('profile')
+        user = User.objects.create_user(**data)
+        PatientInformation.objects.create(
+            user=user,
+            name=profile['name'],
+            dob=profile['dob'],
+            height=profile['height'],
+            weight=profile['weight'],
+            history=profile['history'],
+            allergies=profile['allergies'],
+            patient_wallet=profile['patient_wallet']
+        )
+        user.save()
+        return user
+
 
 class PatientSerializer(serializers.ModelSerializer):
     class Meta:
