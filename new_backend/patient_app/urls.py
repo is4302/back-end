5cc10e297827a507675c0ca40eb09da24dfd2c63
CWei88 <chenwei5lv@gmail.com>
2023-04-10 19:33:17 +0800

    Removed unnecessary files

--- new_backend/patient_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from patient_app.views import PatientRegistrationView, DoctorRegistrationView
-
-urlpatterns = [
-    path('registration/patient', PatientRegistrationView.as_view(), name='patient-registration'),
-    path('registration/doctor', DoctorRegistrationView.as_view(), name='doctor-registration')
-]
